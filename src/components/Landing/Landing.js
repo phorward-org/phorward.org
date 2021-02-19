6142838c48807b2c6e6227c8cd224ad8df47704b
Ryan McHenry <rmchenry@izotope.com>
2021-02-19 10:53:03 -0500

    updating landing

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -9,8 +9,8 @@ export default function Landing() {
   const [line2, setLine2] = useState('')
 
   useEffect(() => {
-    const s1 = 'We build software         '
-    const s2 = 'to help build Philly.   '
+    const s1 = 'Software as a         '
+    const s2 = '*Public Service.   '
     for (const c in s1 + s2) {
       setTimeout(() => {
         if (c < s1.length) {
@@ -33,15 +33,15 @@ export default function Landing() {
         {!mobile && <div id="Cursor2" />}
       </h1>
       <h4>
-        A team of community focused engineers, Phorward supports local
+        A team of community focused engineers, Phorward supports Philadelphia
         nonprofits by designing and developing the software they need - for
         free.
       </h4>
       <div>
-        <Link to="/projects">
+        <Link to="/contact">
           <button>Start a Project</button>
         </Link>
-        <Link to="/about">
+        <Link to="/who">
           <button>Learn More</button>
         </Link>
       </div>
